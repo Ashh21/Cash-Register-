89f9c85f716cbbee69ba2cba247b4ebf43c1b6b6
Ayush shrivastava <106957781+Ashh21@users.noreply.github.com>
2022-09-16 19:21:01 +0530

    Html and java part
    html and logic code done

---
+++ app.js
@@ -0,0 +1,43 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkBtn = document.querySelector("#check-btn");
+const message = document.querySelector("#error-message");
+const availableNotes = [2000, 500, 100, 20, 5, 1];
+const numberOfNotes = document.querySelectorAll(".number-of-notes")
+
+checkBtn.addEventListener("click", function validateBill() {
+    hideMessage();
+    if(billAmount.value > 0){
+        if(cashGiven.value >= billAmount.value) {
+           const amountToBeReturn = cashGiven.value-billAmount.value;
+           calculateChange(amountToBeReturn) ;
+    }
+        else{
+            showMessage("Do you wanna go to police station");
+    }
+    }
+        else{
+            showMessage("Invalid value ");
+        }
+    
+});
+
+function calculateChange (amountToBeReturn){
+    for(let i=0; i<availableNotes.length; i++){
+        const noOfNote = Math.trunc( amountToBeReturn / availableNotes[i]);
+
+        amountToBeReturn =  amountToBeReturn % availableNotes[i];
+
+        numberOfNotes[i].innerText = noOfNote;
+    }
+}
+
+
+function hideMessage (){
+   message.style.display = "none"
+}
+
+function showMessage(msg) {
+    message.style.display = "block";
+    message.innerText = msg;
+}
